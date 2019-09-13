ecf1f08f77cd22f186715e6ac98a7ea72f294906
Sangram Nandkhile <sangram.nandkhile@gmail.com>
2019-09-13 17:21:09 +1000

    Created store module and counter directive

--- SportsStore/src/app/model/product.repository.ts
+++ SportsStore/src/app/model/product.repository.ts
@@ -12,13 +12,17 @@ export class ProductRepository {
             this.categories = data.map(p => p.category)
                 .filter((c, index, array) => array.indexOf(c) == index).sort();
         });
-    } getProducts(category: string = null): Product[] {
+    } 
+    
+    getProducts(category: string = null): Product[] {
         return this.products
             .filter(p => category == null || category == p.category);
     }
+
     getProduct(id: number): Product {
         return this.products.find(p => p.id == id);
     }
+    
     getCategories(): string[] {
         return this.categories;
     }
